3512b3ea981d2f93056d022eb6033f75fb233ee6
Mar G <mgarciaben37@gmail.com>
2025-01-16 11:47:42 +0100

    Add files for public

--- demo-pro/vite.config.ts
+++ demo-pro/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vitest/config";
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
     test: {
         globals: true,
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+            },
+        },
     },
 });
